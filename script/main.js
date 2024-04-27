0415861b43b6cdf34a3de153d61491a45551e4db
Cat Nelson <cwnelson4@wisc.edu>
2024-04-27 13:08:27 -0500

    test

--- script/main.js
+++ script/main.js
@@ -1,4 +1,9 @@
 
 
 let div = document.getElementById("canvas");
-div.innerHTML = '<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';
+let csvform = document.getElementById("fileupload");
+csvform.onsubmit = function() {
+    console.log("submitted");
+}
+div.innerHTML = '<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>' +
+'<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" /></svg>';
